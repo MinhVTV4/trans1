bc49d1fc06b4765b2fd93565cf8042d69d8036d0
MinhVTV4 <trannhatminh50v@gmail.com>
2025-06-17 19:23:59 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ self.addEventListener("install", event => {
     caches.open("v1").then(cache => {
       return cache.addAll([
         "./",
-        "./trans3.html",
+        "./index.html",
         "./manifest.json",
         "./icon-192.png",
         "./icon-512.png"
